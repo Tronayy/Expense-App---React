f186e3005cf8c833bf69ef9bc9e1d1cc05efd198
Tronayy <usmanfazal001@gmail.com>
2021-08-03 21:29:00 +0500

    successfully completed assignment 1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import MakeExpenseItems from "./components/MakeExpenseItems";
+import Expense from "./components/Expense";
 
 function App() {
   const expenses = [
@@ -26,7 +26,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <MakeExpenseItems expenses={expenses} />
+      <Expense expenses={expenses} />
     </div>
   );
 }
